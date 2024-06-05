596966649ae852b6f5352b8bdfb326959a6ed52b
faizhaq1234 <faizhaque44@gmail.com>
2024-06-05 21:12:37 +0530

    HOF and Functional Programming

---
+++ 3.js
@@ -0,0 +1,29 @@
+const items = [
+    {
+        name: "Iphone 15",
+        category: "Mobile",
+        priceUSD: 999,
+    },
+    {
+        name: "Macbook Air",
+        category: "Laptops",
+        priceUSD: 1499,
+    },
+    {
+        name: "Apple Watch",
+        category: "Watches",
+        priceUSD: 499,
+    },
+];
+const exchangeRate = 80;
+
+function convertToINR(priceInUSD) {
+    return priceInUSD * exchangeRate;
+}
+
+const itemsInINR = items.map((item) => ({
+    ...item,
+    priceINR: convertToINR(item.priceUSD),
+}));
+
+console.log(itemsInINR);
